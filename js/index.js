145495e997e5a3ee591a4d72fe27cf0bfb442fea
YanagisawaYuki22 <Tube.dx@gmail.com>
2020-06-05 21:17:49 +0900

    try to include jquery

---
+++ js/index.js
@@ -0,0 +1,9 @@
+<div id="header">
+<nav id="global_navi">
+      <ul>
+        <li><a href="https://yanagisawayuki22.github.io/">トップページ</a></li>
+        <li><a href="./column_archive.html">コラム</a></li>
+        <li><a href="./books_archive.html">ブック</a></li>
+      </ul>
+    </nav>
+</div>
